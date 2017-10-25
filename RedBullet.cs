bb07178350a294ed67027aa874878f2f97ec4640
Justinas Baranauskas <dragomty@inbox.lt>
2017-10-25 15:39:49 +0300

    Add files via upload

---
+++ RedBullet.cs
@@ -0,0 +1,34 @@
+﻿#region Using Statements
+using System;
+using System.Threading;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using GameStateManagement;
+#endregion
+
+namespace GameStateManagementSample
+{
+    public class RedBullet : Bullet//red bullet class
+    {
+        public RedBullet()//allows the bullet to do damagae
+        {
+            isBulletNew = true;
+            setDamage();
+        }
+
+        private void setDamage()//sets the damage
+        {
+            bulletDamage = 100;
+        }
+
+        public override void Update()//moves the bullet
+        {
+            isBulletNew = false;
+            position += new Vector2(20, 0);
+        }
+        
+    }
+}
+
