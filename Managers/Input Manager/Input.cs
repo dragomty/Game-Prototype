c944edc6e7274007372df4164f140d2ce35b7d75
Justinas Baranauskas <dragomty@inbox.lt>
2017-10-27 14:09:55 +0300

    Update Input.cs

--- Managers/Input Manager/Input.cs
+++ Managers/Input Manager/Input.cs
@@ -17,7 +17,7 @@ namespace GameStateManagementSample
 
         }
 
-        //all PlayerIndex code is not currently being used due to the fact that the Paddle is being given as a Listener within the Paddle class
+       
         public static Vector2 GetKeyboardInputDirection()   //PlayerIndex playerIndex)
         {
             Vector2 direction = Vector2.Zero;
@@ -25,8 +25,7 @@ namespace GameStateManagementSample
             KeyboardState keyboardState = Keyboard.GetState();   //playerIndex);
 
             bool keyUp = false;
-            //if (playerIndex == PlayerIndex.One)
-            //{
+           
 
             // statements changed to OR so that the keys do not double up
                 if ((keyboardState.IsKeyDown(Keys.W)) || (keyboardState.IsKeyDown(Keys.Up))) 
@@ -49,30 +48,9 @@ namespace GameStateManagementSample
                     direction.X += 15; // move right
                 }
 
-            //}
+      
 
-            //if (playerIndex == PlayerIndex.Two)
-            //{
-            /*
-                if (keyboardState.IsKeyDown(Keys.Up))
-                {
-                    direction.Y += -10;
-                }
-
-                if (keyboardState.IsKeyDown(Keys.Down))
-                {
-                    direction.Y += 10;
-                }
-                if (keyboardState.IsKeyDown(Keys.Left))
-                {
-                    direction.X += -10;
-                }
-
-                if (keyboardState.IsKeyDown(Keys.Right))
-                {
-                    direction.X += 10;
-                }
-             * */
+           
             return direction;
         }
     }
